b2e5234aeee8ca03f3e617d520152cabd914fce6
Daniel Beaven <dan@Daniels-MacBook-Air.local>
2017-06-09 10:52:33 +0100

    Added scroll wheel controll

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,19 +11,39 @@ class App extends Component {
       people: data.people.map((x, i) => Object.assign({}, x, { _id: i })),
     }
   }
+  componentDidMount() {
+    document.addEventListener('mousewheel', this.handleScroll.bind(this));
+    document.addEventListener('DOMMouseScroll', this.handleScroll.bind(this));
+  }
+  componentWillUnmount() {
+    document.removeEventListener('mousewheel', this.handleScroll);
+    document.removeEventListener('DOMMouseScroll', this.handleScroll);
+  }
+  handleScroll(e) {
+    var e = window.event || e;
+    var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+    if (delta < 0) {
+      if (this.state.selected === 0) return this.setState({ selected: 25 })
+      return this.setState({ selected: this.state.selected - 1})
+    }
+    else {
+      if (this.state.selected === 25) return this.setState({ selected: 0 })
+      return this.setState({ selected: this.state.selected + 1})
+    }
+  }
   handleSelect(name) {
     this.setState({ selected: name })
   }
   render() {
     const { selected, people } = this.state;
     return (
-      <div className="App" style={{ display: 'flex' }}>
-        <Wheel
-          people={people}
-          selected={selected}
-          onSelect={this.handleSelect.bind(this)}
-          radius={20}
-        />
+      <div className="App">
+          <Wheel
+            people={people}
+            selected={selected}
+            onSelect={this.handleSelect.bind(this)}
+            radius={20}
+          />
       </div>
     );
   }
