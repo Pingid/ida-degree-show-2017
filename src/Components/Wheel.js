9976c7f7cdb370001d267cd7b3649a63fed84b5b
Student <student@m-b2406.arts.local>
2017-06-14 16:48:39 +0100

    minor move tab

--- src/Components/Wheel.js
+++ src/Components/Wheel.js
@@ -15,42 +15,42 @@ class Wheel extends Component {
     const { lastSelection } = this.state;
     const theta = (360 / people.length);
     const sliceRotation = (id) => {
-  		if (id < selected) return id * (270 / (people.length - 1)) - 38
-  		if (id === selected) return id * (270 / (people.length - 1))
+      if (id < selected) return id * (270 / (people.length - 1)) - 38
+      if (id === selected) return id * (270 / (people.length - 1))
       return (id * (270 / (people.length - 1))) + 38
     }
-  	const wheelRotation = () => {
-      const lastRotation = -lastSelection * (270 / (people.length - 1));
-      const newRotation = -selected * (270 / (people.length - 1));
-  		return newRotation
-  	}
+    const wheelRotation = () => {
+    const lastRotation = -lastSelection * (270 / (people.length - 1));
+    const newRotation = -selected * (270 / (people.length - 1));
+      return newRotation
+    }
     return (
-  		<Motion style={{ rotation: spring(wheelRotation()) }}>
-  			{ ({ rotation }) => (
-  				<div style={{
-  				 width: `${2 * radius}px`,
-  				 height: `${2 * radius}px`,
-  				 transform: `rotate(${rotation}deg)` }}>
-  			 {
-  				 people.map((x) => {
-  					 const src = require(`../resources/profile-photos/small-cropped/${x.name}.jpg`);
-  					 return (
-  							 <Slice
-  								 key={x._id}
-  								 imageSrc={src}
-  								 radius={radius}
-  								 rotation={sliceRotation(x._id)}
-  								 selected={x._id === selected}
-  								 onSelect={() => onSelect(x._id)}
-  								 theta={theta}
-  								 divisions={people.length}
-  							 />
-  					 )
-  				 })
-  			 }
-  			 </div>
-  			)}
-  		</Motion>
+               <Motion style={{ rotation: spring(wheelRotation()) }}>
+                       { ({ rotation }) => (
+                               <div style={{
+                                width: `${2 * radius}px`,
+                                height: `${2 * radius}px`,
+                                transform: `rotate(${rotation}deg)` }}>
+                        {
+                                people.map((x) => {
+                                        const src = require(`../resources/profile-photos/small-cropped/${x.name}.jpg`);
+                                        return (
+                                                        <Slice
+                                                                key={x._id}
+                                                                imageSrc={src}
+                                                                radius={radius}
+                                                                rotation={sliceRotation(x._id)}
+                                                                selected={x._id === selected}
+                                                                onSelect={() => onSelect(x._id)}
+                                                                theta={theta}
+                                                                divisions={people.length}
+                                                        />
+                                        )
+                                })
+                        }
+                        </div>
+                       )}
+               </Motion>
     )
   }
 }
