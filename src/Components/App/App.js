9d36811c1f6e4575c9a7a4387ea3d5172757f2e2
Daniel Beaven <dan@daniels-air.arts.local>
2017-06-16 15:32:49 +0100

    Added routing

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -17,13 +17,16 @@ const OnMobile = layout.is('mobile');
 const OnDesktop = layout.is('desktop');
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      selected: 0,
       people: data.people.map((x, i) => Object.assign({}, x, { _id: i })),
       scroll: 0,
     }
+    this.getSelected = this.getSelected.bind(this);
+    this.selectPerson = this.selectPerson.bind(this);
+    this.checkNameInURL = this.checkNameInURL.bind(this);
+    this.checkNameInURL(props);
   }
   componentDidMount() {
     if (window.innerWidth > 600) {
@@ -35,28 +38,60 @@ class App extends Component {
     document.removeEventListener('mousewheel', this.handleScroll);
     document.removeEventListener('DOMMouseScroll', this.handleScroll);
   }
+  shouldComponentUpdate(props) { return this.checkNameInURL(props); }
+  checkNameInURL({ history, match }) {
+    const { people } = this.state;
+    if (match.params.name) {
+      const isRealName = people.find(x => (x.profile && x.profile.name.toLowerCase().split(' ').join('-') === match.params.name));
+      if (!isRealName) {
+        history.replace('/');
+        return false;
+      }
+    }
+    return true;
+  }
+  getSelected() {
+    const { match } = this.props;
+    const { people } = this.state;
+    if (!match.params.name) return people[0];
+    return people.find(x => (x.profile && x.profile.name.toLowerCase().split(' ').join('-') === match.params.name));
+  }
+  selectPerson(id) {
+    const { people } =  this.state;
+    const { history } = this.props;
+    const person = people.find(x => x._id === id);
+    const normalisedName = person.profile.name.toLowerCase().split(' ').join('-');
+    return history.push(normalisedName);
+  }
   handleScroll(event) {
-		const { people, selected, scroll } = this.state;
+		const { people, scroll } = this.state;
     const e = window.event || event;
     const delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+    const person = this.getSelected();
+    const selected = person._id;
 
     if (scroll <= -9) {
-      if (selected <= 0) return this.setState({ selected: people.length - 1, scroll: 0 });
-      return this.setState({ selected: selected - 1, scroll: 0 });
+      if (selected <= 0) {
+        this.selectPerson(people.length - 1);
+        return this.setState({ scroll: 0 });
+      }
+      this.selectPerson(selected - 1);
+      return this.setState({ scroll: 0 });
     }
     if (scroll >= 9) {
-      if (selected >= people.length - 1) return this.setState({ selected: 0, scroll: 0 });
-      return this.setState({ selected: selected + 1, scroll: 0 });
+      if (selected >= people.length - 1) {
+        this.selectPerson(0);
+        return this.setState({ scroll: 0 });
+      }
+      this.selectPerson(selected + 1);
+      return this.setState({ scroll: 0 });
     }
 
     if (delta < 0 && e.wheelDelta < -1) { return this.setState({ scroll: scroll - 1}) }
     if (delta > 0 && e.wheelDelta > 1) { return this.setState({ scroll: scroll + 1}) }
   }
-  handleSelect(id) {
-    this.setState({ selected: id })
-  }
   render() {
-    const { selected, people } = this.state;
+    const { people } = this.state;
     const wheelRadius = () => {
       if (window.innerWidth > window.innerHeight) return (window.innerHeight / 2) - 40;
       return (window.innerWidth / 2) - 40;
@@ -67,48 +102,21 @@ class App extends Component {
           <Header />
           <MobileView
             people={people}
-            selected={selected}
-            onSelect={this.handleSelect.bind(this)} />
+            selected={this.getSelected()._id}
+            onSelect={this.selectPerson.bind(this)} />
         </OnMobile>
         <OnDesktop>
-          <div style={{
-            top: 0,
-            position: 'fixed',
-            width: '100vw',
-            height: '100vh',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}>
-            <Wheel
-              people={people}
-              selected={selected}
-              onSelect={this.handleSelect.bind(this)}
-              radius={wheelRadius()}
-            />
+          <div style={{ top: 0, position: 'fixed', width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Wheel people={people} selected={this.getSelected()._id} onSelect={this.selectPerson.bind(this)} radius={wheelRadius()} />
           </div>
-          <div style={{
-            width: '100%',
-            height: '50vh',
-            background: 'white',
-            position: 'fixed',
-            bottom: '0',
-            opacity: '.95'}}>
-          </div>
-          <div style={{
-            position: 'fixed',
-            width: '100%',
-            height: '50vh',
-            bottom: '0',
-            paddingTop: '2rem',
-            boxSizing: 'border-box'
-          }}>
+          <div style={{ width: '100%', height: '50vh', background: 'white', position: 'fixed', bottom: '0', opacity: '.95' }}></div>
+          <div style={{ position: 'fixed', width: '100%', height: '50vh', bottom: '0', paddingTop: '2rem', boxSizing: 'border-box' }}>
             {
-              selected !== 0 ? (
+              this.getSelected()._id !== 0 ? (
                 <Person
                   people={people}
-                  selected={selected}
-                  onSelect={this.handleSelect.bind(this)} />
+                  selected={this.getSelected()._id}
+                  onSelect={this.selectPerson.bind(this)} />
               ) : (
                 <h3 style={{ width: '61.8vw', color: 'black', margin: '0 auto', maxWidth: '46rem', fontWeight: '100' }}>
                   We are the 2017 graduating class of Interaction Design Arts at the London College of Communication. Here you can see our work exhibited at the 360 Degree Show and find links to our other work.
--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { HashRouter as Router, Route } from 'react-router-dom'
 
 import './wheelRotation.css';
-import './slice.css';
 import './index.css';
 
-Math.radians = function(degrees) {
-  return degrees * Math.PI / 180;
-};
+Math.radians = function(degrees) { return degrees * Math.PI / 180; };
+Math.degrees = function(radians) { return radians * 180 / Math.PI; };
 
-Math.degrees = function(radians) {
-  return radians * 180 / Math.PI;
-};
+const routes = () => (
+  <Router>
+    <Route path="/:name?" component={App}/>
+  </Router>
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(routes(), document.getElementById('root'));
 registerServiceWorker();
